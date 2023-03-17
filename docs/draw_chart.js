f6a99605298a7e3d35cbfca46b4353539dbf649d
Victor Hugo <38112094+vhlk@users.noreply.github.com>
2023-03-17 00:23:22 +0000

    Added pie-chart option

--- docs/draw_chart.js
+++ docs/draw_chart.js
@@ -1,4 +1,4 @@
-google.charts.load('current', {'packages':['bar']});
+google.charts.load('current', {'packages':['bar', 'corechart']});
 google.charts.setOnLoadCallback(drawStuff);
 
 function drawStuff() {
@@ -23,20 +23,35 @@ function drawStuff() {
 
     data.sort([{column: 0}]);
 
-    var options = {
-    title: 'Classical Machine Learning Techniques',
-    width: 900,
-    legend: { position: 'none' },
-    chart: { title: 'Classical Machine Learning Techniques' },
-    bars: 'horizontal', // Required for Material Bar Charts.
-    axes: {
-        x: {
-        0: { side: 'bottom', label: 'Usage'} // Top x-axis.
-        }
-    },
-    bar: { groupWidth: "90%" }
-    };
+    var btns = document.getElementById('btn-group');
+    btns.onclick = function (e) {
+        if (e.target.tagName === 'BUTTON') {
+            if (e.target.id === "bar_chart") {
+                var options = {
+                    title: 'Classical Machine Learning Techniques',
+                    width: 900,
+                    legend: { position: 'none' },
+                    chart: { title: 'Classical Machine Learning Techniques' },
+                    bars: 'horizontal', // Required for Material Bar Charts.
+                    axes: {
+                        x: {
+                        0: { side: 'bottom', label: 'Usage'} // Top x-axis.
+                        }
+                    },
+                    bar: { groupWidth: "90%" }
+                };
 
-    var chart = new google.charts.Bar(document.getElementById('chart_div'));
-    chart.draw(data, options);
+                var chart = new google.charts.Bar(document.getElementById('chart_div'));
+                chart.draw(data, options);
+            }
+            if (e.target.id === "pie_chart") {
+                var options = {
+                    title: 'Classical Machine Learning Techniques'
+                  };
+
+                var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+                chart.draw(data, options);
+            }
+        }
+    }
 };
