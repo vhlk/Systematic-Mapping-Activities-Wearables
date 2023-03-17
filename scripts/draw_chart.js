c20dbebfb12ba89ab55b76aa2a813cb8d35cab12
Victor Hugo <38112094+vhlk@users.noreply.github.com>
2023-03-17 20:54:09 +0000

    Showing correct label at X axis

--- scripts/draw_chart.js
+++ scripts/draw_chart.js
@@ -23,7 +23,7 @@ function drawStuff() {
                     bars: 'horizontal', // Required for Material Bar Charts.
                     axes: {
                         x: {
-                        0: { side: 'bottom', label: 'Usage'} // Top x-axis.
+                        0: { side: 'bottom', label: data.bf[1]["label"]} // Top x-axis.
                         }
                     },
                     bar: { groupWidth: "90%" }
